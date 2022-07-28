92c86fff22db47704ca1d5eee81d3111c6fd7a23
MUSTAFA <MUSTAFA@DESKTOP-E7M2JO4>
2022-07-28 12:56:22 +0100

    CRUD Credit

--- PI/PI/src/main/java/tn/esprit/spring/entity/Credit.java
+++ PI/PI/src/main/java/tn/esprit/spring/entity/Credit.java
@@ -22,8 +22,66 @@ public class Credit extends Operation implements Serializable{
 	@Column(name="Montant")
 	private Long montant;
 	
-	  @Temporal(TemporalType.DATE)
-	    @Column(name = "Date", nullable = false)
-	    private Date date;
+	@Temporal(TemporalType.DATE)
+	@Column(name = "Date", nullable = false)
+	private Date date;
+	
+	@Column(name="status")
+	private String status;
+	
+	@Column(name="type")
+	private String type;
+	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getMontant() {
+		return montant;
+	}
+
+	public void setMontant(Long montant) {
+		this.montant = montant;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Credit [id=" + id + ", montant=" + montant + ", date=" + date + ", status=" + status + ", type=" + type
+				+ ", getStatus()=" + getStatus() + ", getType()=" + getType() + ", getId()=" + getId()
+				+ ", getMontant()=" + getMontant() + ", getDate()=" + getDate() + "]";
+	}
+
+
+
+	
+
 
 }
